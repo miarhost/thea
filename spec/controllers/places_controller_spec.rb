69170de8942253cfc74e8e8a2dbe5b1786ec217d
MariaTikhonova <ritz.rkraft@gmail.com>
2018-09-05 16:31:47 +0300

    specs

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -1,26 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe PlacesController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+ 
+ before do
+  it "set place with params" do
+   expect(:valid_attributes).to eql User.place.build(name: "Example", description: "Something by user")
+  end 
+ end
+ 
+ describe "GET #index" do
+  it "returns a success responce" do
+     place = User.places.build! valid_attributes
+     get :index 
+     expect(responce).to be_success
     end
-  end
+   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "POST #create" do
+    context "with valid attributes" do
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "create new Place" do
+     expect {
+      place :create, params: {place: valid_attributes}
+     }
+     expect(response).to be_success
     end
-  end
+
+    it "redirects to created place" do
+      expect {  place :create, params: {place: valid_attributes}}
+      expect(response).to redirect_to(User.place.last)
+     end
+    end
+   end
+
+  describe "POST #update" do
+    context "place with valid attributes exists" do
+      @place = Place.find(params[:id])
+      it "updates existing post and respond success" do
+        expect {
+         post :update, params: {place: valid_attributes}
+        }
+      expect(responce).to assign_to(:place)
+    end
+   end
+ end
 
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 
+ before do
+ 	subject { described_class.new(:name => "Example Place", :description => "Welcome, the first user's place")}
+ end
+
+ describe "ActiveModel validations" do
+ 
+  it "should validate presence of name" do
+  	expect(subject).to validate_presence_of(:title)
+  	expect(response).to be_success
+  end
+
+  it "should validate presense of description" do
+  	expect(subject).to validate_presence_of(:body)
+  end
+  
+ end
+
+ describe "ActiveRecord associations" do
+
+   it "should belong to user" do
+   	expect(subject).to belong_to(:user)
+   end
+
+ end
+
 end
